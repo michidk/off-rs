7bc75b610ff6715816d60f965304f0782a51cef5
Jonas Grawe <Shemnei@users.noreply.github.com>
2022-01-26 22:22:36 +0100

    Fixed and documented fuzz case

--- fuzz/fuzz_targets/try_parse.rs
+++ fuzz/fuzz_targets/try_parse.rs
@@ -1,12 +1,33 @@
+//! A simple fuzz case which tries to parse random strings and checks if the
+//! program (off-rs) panics or runs out of memory.
+//!
+//! # Run
+//!
+//! This will (for now) only work on linux machines.
+//!
+//! Requirements:
+//!
+//! ```bash
+//! cargo install cargo-fuzz
+//! ```
+//!
+//! To run the fuzz case enter the following in a terminal:
+//!
+//! ```bash
+//! cargo fuzz run try_parse
+//! ```
+
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use off_rs::document::ParserOptions;
+use off_rs::mesh::ParserOptions;
 use off_rs::geometry::ColorFormat;
-use off_rs::parser::DocumentParser;
+use off_rs::parser::Parser;
 
+// Creates a new fuzz case which accepts random bytes as input.
 fuzz_target!(|data: &[u8]| {
+    // Tries to interpret the random bytes as string.
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = DocumentParser::new(
+        let _ = Parser::new(
             &s,
             ParserOptions {
                 color_format: ColorFormat::RGBAFloat,
